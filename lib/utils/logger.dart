4e85e4922b46bc3e6707defe8c35022abd83995c
Clarence Ho <clarenceho@gmail.com>
2021-07-31 17:12:36 -0700

    Set log level based on release mode

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,12 +1,12 @@
 import 'dart:developer' as developer;
 
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 final log = new Logger('AndSafe');
 
 void setupLogger() {
-  // Logger.root.level = kReleaseMode? Level.INFO : Level.ALL;
-  Logger.root.level = Level.ALL;
+  Logger.root.level = kReleaseMode? Level.INFO : Level.ALL;
   Logger.root.onRecord.listen((record) {
     developer.log('${record.level.name}: ${record.time}: ${record.message}',
         name: 'net.clarenceho.andsafe');
